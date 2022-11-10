2dfd8ee1b1f1b845ee4ea7953946b12a5a4a6019
pthiagodev@gmail.com <pthiagodev@gmail.com>
2022-11-10 20:57:11 -0300

    Add the project

---
+++ src/main/java/com/thiago/mytestbank/MytestbankApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.mytestbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytestbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytestbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thiago/mytestbank/MytestbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.mytestbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytestbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
